f0fcd93e93febaa967bc45084d3699ef61cd1669
sudheerdarla <sudheer.darla11@gmail.com>
2016-09-18 21:21:37 +0530

    Redesigned website view and added some data options

--- github/main.js
+++ github/main.js
@@ -1,11 +1,15 @@
 function handleProfileData(data) {
   $('.avatar img').attr('src', data.avatar_url);
   $('.name').text(data.name);
+  $('.bio').text(data.bio);
   $('.username').text(data.login);
   $('.location').text(data.location);
   $('.company').text(data.company);
   $('.email a').text(data.email);
   $('.blog a').text(data.blog);
+  $('.blog a').attr('href', data.blog);
+  $('.url a').text(data.html_url);
+  $('.url a').attr('href', data.html_url);
 }
 
 
